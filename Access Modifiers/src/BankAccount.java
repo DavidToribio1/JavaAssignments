d7bfb3af56dbd132804573169ca4ff07231da73e
DavidToribio1 <72708052+DavidToribio1@users.noreply.github.com>
2022-03-21 13:06:28 -0700

    First Commit

---
+++ Access Modifiers/src/BankAccount.java
@@ -0,0 +1,24 @@
+
+public class BankAccount {
+	
+	private	int accountNumber;
+	private double balance;
+	public String type;
+	
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		
+		this.balance = balance;
+	}
+
+}
